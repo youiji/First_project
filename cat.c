389cad56509916065b16f7ed1224e9d4b090bd7e
Mandarin Raul <mandarra@oa-b5.msk.21-school.ru>
2026-04-20 12:02:45 +0300

    task 1.1

--- cat.c
+++ cat.c
@@ -7,14 +7,27 @@ typedef struct {
     bool n;
     bool s;
     bool t;
+    bool v;
 
 }Flags;
 
 int main(int argc, char *argv[]) {
     Flags flags = {0};
     parcing_flags(argc, argv, &flags);
-    for (int i = optind; i < argc; ++i){
-        print_file(argv[i], flags);
+    for (int i = 1; i < argc; ++i){
+        print_file(argv[i]);
     }
     return 0;
+}
+
+void print_file(char *name){
+    FILE *f = fopen(name, "rt");
+    if (f != NULL) {
+        int c = fgetc(f);
+        while (c != EOF) {
+            putc(c, stdout);
+            c = fgetc(f);
+        }
+        fclose(f);
+    }
 }
